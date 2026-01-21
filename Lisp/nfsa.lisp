edfa2aad7bd11fb07a281ac1989b39d2cf9c033a
Gioele Bertinelli <gioelebertinelli@gmail.com>
2026-01-21 03:39:56 +0100

    Refactor comments in nfsa.lisp for improved clarity and consistency in regex validation logic

--- Lisp/nfsa.lisp
+++ Lisp/nfsa.lisp
@@ -12,48 +12,47 @@
   
   (cond
    
-   ;; Caso lista vuota: non e' una regex valida
+   ; Caso lista vuota: non e' una regex valida
    ((null RE) nil)
    
-   ;; Caso base: un atomo (simbolo o numero) e' sempre una regex valida
-   ;; Questo include anche 'a, 'c, 'z, 'o quando sono usati come simboli
+   ;; Caso atomo: e' sempre una regex valida 
+   ;; (anche 'a, 'c, 'z, 'o quando sono usati come simboli)
    ((atom RE) t)
    
-   ;; Caso lista: dobbiamo controllare se e' un operatore valido
+   ;; Caso lista: controllo se l'operatore è valido
    ((listp RE)
     (cond
      
-     ;; Operatore 'c' (sequenza): deve avere almeno un argomento
+     ;; Caso operatore 'c' (sequenza): deve avere almeno un argomento
      ;; e tutti gli argomenti devono essere regex valide
      ((eq (first RE) 'c)
-      (and (>= (length (rest RE)) 1)  ; almeno un argomento
-           (every #'is-regex (rest RE))))  ; controllo ricorsivo
+      (and (>= (length (rest RE)) 1)  
+           (every #'is-regex (rest RE))))
      
-     ;; Operatore 'a' (alternativa): deve avere almeno un argomento
+     ;; Caso operatore 'a' (alternativa): deve avere almeno un argomento
      ;; e tutti gli argomenti devono essere regex valide
      ((eq (first RE) 'a)
       (and (>= (length (rest RE)) 1)
            (every #'is-regex (rest RE))))
      
-     ;; Operatore 'z' (stella di Kleene): deve avere esattamente un argomento
-     ;; che deve essere una regex valida
+     ;; Caso operatore 'z' (chiusura di Kleene):
+     ;; deve avere esattamente un argomento che deve essere una regex valida
      ((eq (first RE) 'z)
-      (and (= (length (rest RE)) 1)  ; esattamente un argomento
-           (is-regex (second RE))))  ; controllo ricorsivo
+      (and (= (length (rest RE)) 1)  
+           (is-regex (second RE))))
      
-     ;; Operatore 'o' (uno-o-piu'): deve avere esattamente un argomento
-     ;; che deve essere una regex valida
+     ;; Caso operatore 'o' (ripetizione uno-o-piu'):
+     ;; deve avere esattamente un argomento che deve essere una regex valida
      ((eq (first RE) 'o)
       (and (= (length (rest RE)) 1)
            (is-regex (second RE))))
      
      ;; Se il primo elemento NON e' uno degli operatori riservati (c, a, z, o),
-     ;; allora questa e' una S-expression che rappresenta un simbolo dell'alfabeto
-     ;; e quindi e' una regex valida
-     ;; Esempio: (foo bar) e' un simbolo valido dell'alfabeto
+     ;; allora questa e' una S-expression che rappresenta un simbolo
+     ;; dell'alfabeto e quindi e' una regex valida. Esempio: (foo bar) 
      (t t)))
    
-   ;; Altri casi (per evitare bug)
+   ;; Altri casi: catch-all per tipi non previsti
    (t nil)))
 
 
